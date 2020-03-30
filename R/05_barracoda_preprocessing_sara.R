0965a034cf702ea019dead49c1bc47b32ddc9e99
Sara Suarez Hernandez <you@example.com>
2020-03-30 13:13:39 +0200

    sara data preprocessing code

--- R/05_barracoda_preprocessing_sara.R
+++ R/05_barracoda_preprocessing_sara.R
@@ -1,30 +1,77 @@
+# Script explanation
+# This scripts aims to pre-processed the raw barracoda output by:
+## - Adding a response == yes / no column.
+## - Adding a treatment == yes / no column.
+
+# Clear workspace
+# ------------------------------------------------------------------------------
+rm(list = ls())
+
+# Load libraries
+# ------------------------------------------------------------------------------
 library(tidyverse)
 library(readxl)
 library(writexl)
 
-file.choose()
-## Load multiple sheets into one single dataframe
-path <- "data/raw/barracoda_output_CT26.xlsx"
+# Load data
+# -----------------------------------------------------------------------------
+## The raw barracoda output comprises one sheet per sample. We will import all sheets into a unique dataframe.
+path_ct26 <- "data/raw/barracoda_output_CT26.xlsx"
 
-all_ct26_barracoda <- path %>% 
+all_ct26_barracoda_raw <- path_ct26 %>% 
   excel_sheets() %>% 
   set_names() %>% 
-  map_df(~ read_excel(path = path, sheet = .x), .id = "sheet")
+  map_df(~ read_excel(path = path_ct26, sheet = .x), .id = "sheet")
 
+path_4t1 <- "data/raw/barracoda_output_4T1.xlsx"
 
-## Add response column based on cut-off: Log2>2 & p-value<0.05
-ct26_barracoda <- all_ct26_barracoda %>% mutate(response = case_when(
-    log_fold_change >= 2 & p <= 0.05 & count.1 >= input.1 ~ "yes",
+all_4t1_barracoda_raw <- path_4t1 %>% 
+  excel_sheets() %>% 
+  set_names() %>% 
+  map_df(~ read_excel(path = path_4t1, sheet = .x), .id = "sheet")
+
+# Wrangle data
+# ------------------------------------------------------------------------------
+## Add response column based on cut-off: 
+all_ct26_barracoda <- all_ct26_barracoda_raw %>% 
+  mutate(response = case_when(
+    # counts of a response are enriched compared to input (barcode added to sample)
+    count.1 < input.1 ~ "count<input", 
+    # yes response if logfold & pvalue conditions match & there is input in all three triplicates 
+    log_fold_change >= 2 & p <= 0.05 & input.1&input.2&input.3 != 0   ~ "yes",
+    # everything else does not match any of the previous criterias as is labelled no
     TRUE ~ "no")) %>% 
-  mutate(identifier = paste(HLA, Peptide.name, sep="_")) 
+  mutate(treatment = case_when(
+    sample == "CT26_C1" | sample == "CT26_D1" | sample == "CT26_D2" ~ "yes",
+    sample == "CT26_C3" | sample == "CT26_C4" | sample == "CT26_D4" ~ "no"
+    )) %>% 
+  mutate(organ="spleen") %>% 
+  mutate(identifier = paste(HLA, Peptide.name, sep="_"))
 
-responses <- all_ct26_barracoda %>% filter(response=="yes") %>% distinct(identifier, .keep_all = T)
+all_ct26_barracoda %>% 
+  filter(response=="yes") %>% 
+  distinct(Peptide.name, .keep_all = T) #19 unique peptides triggered responses 
 
-# Sample de-selected because when choosing uniques identifiers, all come from C1. 
-ct26_barracoda <- ct26_barracoda %>% 
-  group_by(sample) %>% 
-  distinct(identifier, .keep_all = T)
+all_4t1_barracoda <- all_4t1_barracoda_raw %>% 
+  select(-`...17`) %>% 
+  mutate(response = case_when(
+    # counts of a response are enriched compared to input (barcode added to sample)
+    count.1 < input.1 ~ "count<input", 
+    # yes response if logfold & pvalue conditions match & there is input in all three triplicates 
+    log_fold_change >= 2 & p <= 0.05 & input.1&input.2&input.3 != 0   ~ "yes",
+    # everything else does not match any of the previous criterias as is labelled no
+    TRUE ~ "no")) %>% 
+  mutate(treatment = case_when(
+    str_detect(sample, "^4T1_19") | str_detect(sample,"^4T1_23") | str_detect(sample,"^4T1_20") | str_detect(sample,"^4T1_16") ~ "yes",
+    str_detect(sample,"^4T1_22") | str_detect(sample,"^4T1_17") | str_detect(sample,"^4T1_18") ~ "no",
+    TRUE ~ as.character(sample))) %>% 
+  mutate(organ = case_when(
+    str_detect(sample, "TU$") ~ "tumor",
+    str_detect(sample, "SP$") ~ "spleen"
+    )) %>% 
+  mutate(identifier = paste(HLA, Peptide.name, sep="_"))
 
-write_xlsx(ct26_responses, "data/ct26_responses_barracoda.xlsx")
+all_4t1_barracoda %>% 
+  filter(response=="yes") %>% 
+  distinct(Peptide.name, .keep_all = T) #14 unique peptides triggered responses 
 
-ct26_responses %>% group_by(sample, response) %>% count() 
