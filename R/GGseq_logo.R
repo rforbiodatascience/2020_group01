a3aa6b8c3ebf3118cfb7790ccd9451b7e6a18efb
Annie Borch <annbor@DTU-C02YM2PFJHD2.local>
2020-04-30 11:31:14 +0200

    change ggseqlogo scriot

--- R/GGseq_logo.R
+++ R/GGseq_logo.R
@@ -1,1 +1,13 @@
-# 
+library(ggplot2)
+#install.packages(ggseqlogo)
+library(ggseqlogo)
+
+my_data_clean_aug %>% 
+  filter(str_length(Mut_peptide)==9,response=="yes",Mutation_Consequence=="M") %>% 
+  select(Mut_peptide) %>% 
+  ggseqlogo()
+
+my_data_clean_aug %>% 
+  filter(str_length(Mut_peptide)==9,response=="no",Mutation_Consequence=="M") %>% sample_n(20) %>% 
+  select(Mut_peptide) %>% 
+  ggseqlogo()
