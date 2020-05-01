d27ded291caadddf1a04eb778152c53f9e38d3b5
Sara Suárez <sarasuarez@dtu-5dh9hv1.win.dtu.dk>
2020-05-01 14:17:13 +0200

    03_agument real final

--- R/03_augment.R
+++ R/03_augment.R
@@ -18,9 +18,20 @@ my_data_clean <- read_tsv(file = "data/02_my_data_clean.tsv")
 # Wrangle data
 # ------------------------------------------------------------------------------
 my_data_clean_aug <- my_data_clean %>% 
+  # add response column
   mutate(response = case_when(log_fold_change >= 2 & p_value <= 0.01 & input_1&input_2&input_3 != 0 & count > input_1  ~ "yes",
                               # everything else does not match any of the previous criterias as is labelled no
                               TRUE ~ "no")) %>% 
+  # add organ column
+  mutate(organ = case_when(str_detect(sample, "TU$") ~ "tumor",
+                           str_detect(sample, "SP$") | str_detect(sample, "^CT26") ~ "spleen")) %>% 
+  
+  # add treatment column
+  mutate(treatment = case_when(str_detect(sample, "^4T1_19") | str_detect(sample,"^4T1_23") | str_detect(sample,"^4T1_20") | 
+                                 str_detect(sample,"^4T1_16") | sample == "CT26_C1" | sample == "CT26_D1" | sample == "CT26_D2" ~ "yes",
+                               str_detect(sample,"^4T1_22") | str_detect(sample,"^4T1_17") | str_detect(sample,"^4T1_18") | 
+                                 sample == "CT26_C3" | sample == "CT26_C4" | sample == "CT26_D4" ~ "no")) %>% 
+  
   # add cell_line column
   mutate(cell_line = case_when(str_detect(sample, "^4T1") ~ "4T1", str_detect(sample, "^CT26") ~ "CT26")) %>% 
   # add percent_count.fraction column = barcode_count / sum(barcode_counts) for a particular sample * 100
