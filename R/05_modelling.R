f859da426431688c19afd2e62a85f0333907f2af
Annie Borch <annbor@kt-co-chuqi-po.win.dtu.dk>
2020-05-03 14:49:44 +0200

    make updates in modellling

--- R/05_modelling.R
+++ R/05_modelling.R
@@ -1,21 +1,14 @@
 
 #### modelling 
-TEMPT <- my_data_clean_aug %>% 
-  mutate(new_score_2  = expression_level/(mut_mhcrank_el+self_similarity))
-
-
 my_data_clean_aug <- my_data_clean_aug %>% 
   mutate(new_score  = expression_level/(mut_mhcrank_el+self_similarity))
-max(my_data_clean_aug$mut_mhcrank_el)
-my_data_clean_aug <- my_data_clean_aug %>% 
-  mutate(new_score  = norm_mhcrank_el/(mut_mhcrank_el))
 
 my_data_clean_aug %>% 
   ggplot(aes( x = priority_score )) + 
   geom_line(stat = "count") + 
   facet_grid(.~response) 
 
-TEMPT  %>% 
+my_data_clean_aug  %>% 
   ggplot(aes( x = new_score, y = self_similarity )) + 
   geom_point()+
   facet_grid(.~response) +
@@ -28,20 +21,10 @@ library(dlstats)    # for package download stats
 library(pkgsearch)
 library(ROCR)
 
-yes_df <- TEMPT %>% filter(response=="yes")
-no_df <- TEMPT %>% filter(response=="no") %>% sample_n(50)
-
-df <- rbind(yes_df,no_df )
-
-TEMPT_without_dup <- subset(TEMPT, TEMPT$cell_line=="CT26")
-
-table(my_data_clean_aug_moddeling$label)
-
 my_data_clean_aug$label <-  ifelse(my_data_clean_aug$response=="yes", 1,0)
-my_data_clean_aug_moddeling <- my_data_clean_aug %>% filter(new_score < 3 )
 pred <- prediction(my_data_clean_aug$allele_frequency, my_data_clean_aug$label)
 perf <- performance(pred,"tpr","fpr")
-plot(perf,colorize=TRUE)
+ROC_allele_freq <- plot(perf,colorize=TRUE)
 
 
 
