8a41674e54e5db63cab841a94b997d084c7762f3
Annie Borch <annbor@kt-co-chuqi-po.win.dtu.dk>
2020-05-03 21:08:43 +0200

    Make fast html with all plots

--- R/04_analysis.R
+++ R/04_analysis.R
@@ -9,6 +9,7 @@ library(ggrepel)
 library(ggplot2)
 #install.packages(ggseqlogo)
 library(ggseqlogo)
+library(ggseqlogo)
 library(cowplot)
 
 # Define functions
@@ -23,7 +24,7 @@ my_data_clean_aug <- read_tsv(file = "data/03_my_data_clean_aug.tsv")
 # Wrangle data
 # ------------------------------------------------------------------------------
 # select unique peptides 
-P1 <- data_single_peptides <- my_data_clean_aug %>% 
+data_single_peptides <- my_data_clean_aug %>% 
   group_by(response) %>% 
   distinct(identifier, .keep_all = T)
 
@@ -148,34 +149,34 @@ P13 <- my_data_clean_aug %>%
 
 
 ################## GGseq logo 
-# ------------------------------------------------------------------------------------------
-#plot seq log, there only exist responses in length 9 to 10 so that is the only one illustarted
-plot_List <- list()
-i=1
-for (num in 9:10) {
-  for(r in c("yes","no"))  {
-    p <- seqloggo_generator(my_data_clean_aug, num, r)
-    print(p)
-    plot_List[[i]] <- p
-    i <- i+1
-  }
-}
-
-# plot together
-pdf(file = "Results/GGseq_plot.pdf", width = 12, height = 6)
-ggdraw() +
-   draw_plot(plot_List[[1]], 0, .51, .45, .4) +
-   draw_plot(plot_List[[2]], 0, .0, .45, .4) +
-   draw_plot(plot_List[[3]], .47, .51, .45, .4) +
-   draw_plot(plot_List[[4]], .47, .0, .45, .4) +
-   draw_plot_label(c("Responses", "No responses"), c(-0.05,  -0.06), c(1,  .48), size = 22)
-dev.off()
-
-# exploring data with shiny app 
-# ------------------------------------------------------------------------------
-source(file = "R/shiny_app.R")
-Exploring_data_shiny(my_data_clean_aug)
-
+# # ------------------------------------------------------------------------------------------
+# #plot seq log, there only exist responses in length 9 to 10 so that is the only one illustarted
+# plot_List <- list()
+# i=1
+# for (num in 9:10) {
+#   for(r in c("yes","no"))  {
+#     p <- seqloggo_generator(my_data_clean_aug, num, r)
+#     print(p)
+#     plot_List[[i]] <- p
+#     i <- i+1
+#   }
+# }
+# 
+# # plot together
+# pdf(file = "Results/GGseq_plot.pdf", width = 12, height = 6)
+# ggdraw() +
+#    draw_plot(plot_List[[1]], 0, .51, .45, .4) +
+#    draw_plot(plot_List[[2]], 0, .0, .45, .4) +
+#    draw_plot(plot_List[[3]], .47, .51, .45, .4) +
+#    draw_plot(plot_List[[4]], .47, .0, .45, .4) +
+#    draw_plot_label(c("Responses", "No responses"), c(-0.05,  -0.06), c(1,  .48), size = 22)
+# dev.off()
+# 
+# # exploring data with shiny app 
+# # ------------------------------------------------------------------------------
+# source(file = "R/shiny_app.R")
+# Exploring_data_shiny(my_data_clean_aug)
+# 
 
 # Write data
 # ------------------------------------------------------------------------------
--- R/99_project_functions.R
+++ R/99_project_functions.R
@@ -2,15 +2,15 @@
 # ------------------------------------------------------------------------------
 
 
-### seq logo generater 
+### seq logo generater
 # seqloggo_generator <-  function(data = my_data_clean_aug ,
 #                                 len = 9,
 #                                 resp = c("yes","no"),
 #                                 cons = c("F","M","I","D"),
-#                                 mouse = c("CT26","4T1")) 
-# { p <- my_data_clean_aug %>% 
-#     filter(str_length(mut_peptide)==len,response==resp,mutation_consequence==cons,cell_line == mouse) %>% 
-#     select(mut_peptide) %>% 
+#                                 mouse = c("CT26","4T1"))
+# { p <- my_data_clean_aug %>%
+#     filter(str_length(mut_peptide)==len,response==resp,mutation_consequence==cons,cell_line == mouse) %>%
+#     select(mut_peptide) %>%
 #     ggseqlogo()
 # return(p)
 # }
