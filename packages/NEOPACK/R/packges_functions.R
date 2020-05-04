8a100266ed02020e438d88f822d848778a203931
Annie Borch <annbor@dtu-czc7077yz0.win.dtu.dk>
2020-05-04 06:43:49 +0200

    update packages

--- packages/NEOPACK/R/packges_functions.R
+++ packages/NEOPACK/R/packges_functions.R
@@ -1,2 +1,27 @@
 # packges functions
 
+
+#' Multiply function
+#'
+#' This function can make par plots
+#' @param num
+#' @keywords number
+#' @export
+#' @examples
+#' cat_function()
+bar_plot_func <- function(num) {
+  my_data_clean_aug %>%
+    filter(str_length(mut_peptide)==num,mutation_consequence=="M") %>%
+    ggplot(aes(x=peptide_position)) +
+    geom_bar(aes(fill = response), stat = "count")+
+    scale_y_log10() +
+    scale_x_discrete(limits = c(1:num)) +
+    theme_bw()
+
+}
+bar_plot_func(9)
+
+setwd("/Users/annbor/Documents/courses/R_course_22100/2020_group01_project/packages/NEOPACK")
+document()
+
+
